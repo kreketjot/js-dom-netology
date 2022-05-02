7212a8e51ee3ece46f36fd86ca967cdf2a1a85c1
kreketjot <kreketjot@users.noreply.github.com>
2022-05-02 21:15:44 +0000

    Deploying to gh-pages from @ kreketjot/js-dom-netology@25944dfe72bfb280bdf443277ce3108da5bb03bc 🚀

---
+++ beae53176f323396c385.js
@@ -0,0 +1,1 @@
+import setNodeInNewPlace from"./setNodeInNewPlace";const goblin=document.getElementById("goblin"),places=document.getElementsByClassName("grid-item");let curPlace=null;const updateGoblin=()=>{curPlace=setNodeInNewPlace(places,curPlace,goblin)};updateGoblin(),setInterval(updateGoblin,700);
--- f1bcd85531bcef033a5d.js
+++
@@ -1,1 +0,0 @@
-const goblin=document.getElementById("goblin");
